33ec34e1c266af6b8e075331a5b8f75898702a90
ivaylo91 <ipenev91@gmail.com>
2023-01-12 17:08:29 +0200

    Print Square Of Stars

---
+++ 01_Syntax_Statement_Functions/09_Square_Stars.js
@@ -0,0 +1,11 @@
+function printSquare(n){
+    let string = "";
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            string += "* ";
+        }
+        string += '\n';
+    }
+    console.log(string);
+}
+printSquare(5);
