fd9f56dbfb8fa4be4d9b740abe1e2286dc6ac6ee
ivaylo91 <ipenev91@gmail.com>
2023-01-17 12:09:57 +0200

    find greatest common divisor

---
+++ 02_Exercises_Syntax_Statement/02_Greatest_Common_Divisor.js
@@ -0,0 +1,21 @@
+function findGCD(num1, num2) {
+    num1 = Math.abs(num1);
+    num2 = Math.abs(num2);
+
+    if (num2 > num1) {
+        let temp = num1;
+        num1 = num2
+        num2 = temp;
+    }
+    while (true) {
+        if (num2 === 0)
+            return num2;
+        num1 %= num2;
+        if (num1 === 0)
+            return num2;
+        num2 %= num1;
+    }
+}
+
+console.log(findGCD(15, 5));
+console.log(findGCD(2154,458));
