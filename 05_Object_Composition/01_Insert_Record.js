94a9bc1c1a0187f73c619e9ea27734f3f0ac7ef4
ivaylo91 <ipenev91@gmail.com>
2023-02-08 12:26:55 +0200

    return object function

---
+++ 05_Object_Composition/01_Insert_Record.js
@@ -0,0 +1,9 @@
+function insert(name,population,treasury){
+    return {
+        name:name,
+        population:population,
+        treasury:treasury
+    };
+}
+
+console.log(insert('Tortuga', 7000, 15000));
